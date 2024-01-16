1a1e3a5dd17c8464a1400f4dc0131c0d6e5c73d0
Rfaelsn <rafaelnascimentovf6@gmail.com>
2024-01-16 00:26:00 -0300

    feat[main]: adicionando endereço do vercel no cors da api

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://front-estagio-legal-z2et.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
