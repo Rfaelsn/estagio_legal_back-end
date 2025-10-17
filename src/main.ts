336e7c2827d256fee811a15b6f183e7b145f2e35
Rfaelsn <rafaelnascimentovf6@gmail.com>
2025-10-16 22:34:43 -0300

    build: ajuste de cors

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://front-a1tcjzaro-estagio-legal.vercel.app'],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: [
-      'Content-Type',
-      'Accept',
-      'Authorization',
-      'X-Requested-With',
-    ],
+    origin: true,
     credentials: true,
   });
 
