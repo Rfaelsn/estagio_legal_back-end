0ed4af21535028323bfb70f3811bb7a991153cba
Rfaelsn <rafaelnascimentovf6@gmail.com>
2025-10-16 22:38:53 -0300

    build: ajuste de cors

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
