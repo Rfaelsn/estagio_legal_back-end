273afe3c01e67f58fd0d5e59fec8486ca2b5cb08
Rfaelsn <rafaelnascimentovf6@gmail.com>
2025-10-30 19:00:35 -0300

    build: ajuste postbuild

--- postbuild.bash
+++ postbuild.bash
@@ -1,13 +1,5 @@
-#!/bin/bash
+echo -e "Store puppeteer executable in cache\n"
 
-echo 'Preparing to run automation on production mode.'
+mkdir ./.cache
 
-puppeteer_cache_dir=".cache/puppeteer/chrome/"
-puppeteer_dep_dir="node_modules/puppeteer/"
-
-if [[ ! -d "$puppeteer_cache_dir" ]]; then
-    echo "$puppeteer_cache_dir directory does not exist. Trying to re-install Puppeteer."
-    npm install puppeteer
-fi
-
-echo "Puppeteer postbuild finalizado."
+mv /app/.cache/puppeteer ./.cache
