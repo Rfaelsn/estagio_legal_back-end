64f0af7e0cf780157543e6046793086908b444f2
Rfaelsn <rafaelnascimentovf6@gmail.com>
2025-10-30 18:38:24 -0300

    build: ajustando bash postbuild para teste com puppeter

--- postbuild.bash
+++ postbuild.bash
@@ -1,5 +1,20 @@
-echo -e "Store puppeteer executable in cache\n"
+#!/bin/bash
 
-mkdir ./.cache
+echo 'Preparing to run automation on production mode.'
 
-mv /app/.cache/puppeteer ./.cache
+puppeteer_cache_dir=".cache/puppeteer/chrome/"
+
+puppeteer_dep_dir="node_modules/puppeteer/"
+
+if [[ -d "$puppeteer_cache_dir" ]];
+then
+    echo "$puppeteer_cache_dir directory exists."
+else
+	echo "$puppeteer_cache_dir directory does not exist. Trying to re-install Puppeteer."
+    cd "$puppeteer_dep_dir"
+    npm install
+fi
+
+echo "Going back to original directory and running script now..."
+cd
+npm run my_puppeteer_routine_here
